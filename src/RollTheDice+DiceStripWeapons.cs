2472bca3eb1cf4b6f9f9e8e7800424b1b1112c2b
Kalle <kalle@kandru.de>
2025-02-10 17:45:44 +0100

    fix: strip weapons properly (without ui bug for the bomb)

--- src/RollTheDice+DiceStripWeapons.cs
+++ src/RollTheDice+DiceStripWeapons.cs
@@ -1,10 +1,29 @@
 using CounterStrikeSharp.API.Core;
 using CounterStrikeSharp.API.Modules.Entities.Constants;
+using CounterStrikeSharp.API.Modules.Utils;
 
 namespace RollTheDice
 {
     public partial class RollTheDice : BasePlugin
     {
+        private List<string> _weaponsToKeep = [
+            CsItem.C4.ToString().ToLower(),
+            $"weapon_{CsItem.C4.ToString().ToLower()}",
+            CsItem.Bomb.ToString().ToLower(),
+            $"weapon_{CsItem.Bomb.ToString().ToLower()}",
+            "knife",
+            "weapon_knife",
+            CsItem.Knife.ToString().ToLower(),
+            $"weapon_{CsItem.Knife.ToString().ToLower()}",
+            CsItem.KnifeCT.ToString().ToLower(),
+            $"weapon_{CsItem.KnifeCT.ToString().ToLower()}",
+            CsItem.KnifeT.ToString().ToLower(),
+            $"weapon_{CsItem.KnifeT.ToString().ToLower()}",
+            CsItem.DefaultKnifeCT.ToString().ToLower(),
+            $"weapon_{CsItem.DefaultKnifeCT.ToString().ToLower()}",
+            CsItem.DefaultKnifeT.ToString().ToLower(),
+            $"weapon_{CsItem.DefaultKnifeT.ToString().ToLower()}",
+        ];
         private Dictionary<string, string> DiceStripWeapons(CCSPlayerController player, CCSPlayerPawn playerPawn)
         {
             if (playerPawn.WeaponServices == null)
@@ -12,27 +31,35 @@ namespace RollTheDice
                 {
                     {"error", "command.rollthedice.error"}
                 };
-            var playerWeapons = playerPawn.WeaponServices!;
-            bool hasC4 = false;
-            foreach (var weapon in playerWeapons.MyWeapons)
+            // get weapons to remove
+            List<nint> myWeapons = [];
+            foreach (var weapon in playerPawn.WeaponServices.MyWeapons)
             {
                 // ignore unknown weapons
-                if (weapon.Value == null || weapon.Value != null && weapon.Value.DesignerName == null) continue;
-                if (weapon.Value!.DesignerName == CsItem.C4.ToString().ToLower()
-                    || weapon.Value!.DesignerName == CsItem.Bomb.ToString().ToLower()
-                    || weapon.Value!.DesignerName == $"weapon_{CsItem.C4.ToString().ToLower()}"
-                    || weapon.Value!.DesignerName == $"weapon_{CsItem.Bomb.ToString().ToLower()}")
+                if (weapon == null || weapon.Value == null || weapon.Value.DesignerName == null) continue;
+                // remove all other weapons
+                if (!_weaponsToKeep.Contains(weapon.Value!.DesignerName.ToLower()))
                 {
-                    hasC4 = true;
+                    myWeapons.Add(weapon.Value!.Handle);
                 }
             }
-            player.RemoveWeapons();
-            player.GiveNamedItem(CsItem.Knife);
-            if (hasC4)
-                player.GiveNamedItem(CsItem.C4);
+
+            foreach (var entry in myWeapons)
+            {
+                // get weapon from raw handle
+                CBasePlayerWeapon weapon = new CBasePlayerWeapon(entry);
+                if (weapon == null || !weapon.IsValid) continue;
+                // set player active weapon
+                playerPawn.WeaponServices.ActiveWeapon.Raw = weapon.EntityHandle;
+                // drop active weapon
+                player.DropActiveWeapon();
+                // remove weapon
+                weapon.AcceptInput("kill");
+            }
+
             return new Dictionary<string, string>
             {
-                { "playerName", player.PlayerName }
+                { "playerName", player.PlayerName}
             };
         }
     }
