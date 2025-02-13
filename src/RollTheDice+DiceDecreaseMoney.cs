7079a20d1b161441ac85d9f48fbd3ecffdca3e57
Kalle <kalle@kandru.de>
2025-02-13 13:02:03 +0100

    bugfix: bounds check for IncreaseMoney and DecreaseMoney (#4)

--- src/RollTheDice+DiceDecreaseMoney.cs
+++ src/RollTheDice+DiceDecreaseMoney.cs
@@ -8,11 +8,21 @@ namespace RollTheDice
         private Dictionary<string, string> DiceDecreaseMoney(CCSPlayerController player, CCSPlayerPawn playerPawn)
         {
             Dictionary<string, object> config = GetDiceConfig("DiceDecreaseMoney");
+            // decrease money randomly
             var moneyDecrease = _random.Next(
                 Convert.ToInt32(config["min_money"]),
                 Convert.ToInt32(config["max_money"]) + 1
             );
-            player.InGameMoneyServices!.Account -= moneyDecrease;
+            // bounds check
+            if (player.InGameMoneyServices!.Account - moneyDecrease >= 0)
+            {
+                player.InGameMoneyServices!.Account -= moneyDecrease;
+            }
+            else
+            {
+                player.InGameMoneyServices!.Account = 0;
+            }
+            // update player state
             Utilities.SetStateChanged(player, "CCSPlayerController", "m_pInGameMoneyServices");
             return new Dictionary<string, string>
             {
--- src/RollTheDice+DiceIncreaseMoney.cs
+++ src/RollTheDice+DiceIncreaseMoney.cs
@@ -1,5 +1,6 @@
 using CounterStrikeSharp.API;
 using CounterStrikeSharp.API.Core;
+using CounterStrikeSharp.API.Modules.Cvars;
 
 namespace RollTheDice
 {
@@ -8,11 +9,28 @@ namespace RollTheDice
         private Dictionary<string, string> DiceIncreaseMoney(CCSPlayerController player, CCSPlayerPawn playerPawn)
         {
             Dictionary<string, object> config = GetDiceConfig("DiceIncreaseMoney");
+            // get server configured maximum amount of money
+            int maxMoney = 0;
+            ConVar? mpMaxMoney = ConVar.Find("mp_maxmoney");
+            if (mpMaxMoney != null)
+            {
+                maxMoney = mpMaxMoney.GetPrimitiveValue<int>();
+            }
+            // increase money randomly
             var moneyIncrease = _random.Next(
                 Convert.ToInt32(config["min_money"]),
                 Convert.ToInt32(config["max_money"]) + 1
             );
-            player.InGameMoneyServices!.Account += moneyIncrease;
+            // bounds check
+            if (player.InGameMoneyServices!.Account + moneyIncrease <= maxMoney)
+            {
+                player.InGameMoneyServices!.Account += moneyIncrease;
+            }
+            else
+            {
+                player.InGameMoneyServices!.Account = maxMoney;
+            }
+            // update player state
             Utilities.SetStateChanged(player, "CCSPlayerController", "m_pInGameMoneyServices");
             return new Dictionary<string, string>
             {
