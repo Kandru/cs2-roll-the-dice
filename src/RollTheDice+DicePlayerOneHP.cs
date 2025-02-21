8a291c6001293038cd48685e7e02cb98971ee583
Kalle <kalle@kandru.de>
2025-02-21 14:27:01 +0100

    fix: reset DicePlayerOneHP for the user themself

--- src/RollTheDice+DicePlayerOneHP.cs
+++ src/RollTheDice+DicePlayerOneHP.cs
@@ -7,12 +7,25 @@ namespace RollTheDice
     {
         private Dictionary<string, string> DicePlayerOneHP(CCSPlayerController player, CCSPlayerPawn playerPawn)
         {
+            // set maximum health to players last known health value
+            playerPawn.MaxHealth = playerPawn.Health;
+            // set health to 1
             playerPawn.Health = 1;
             Utilities.SetStateChanged(playerPawn, "CBaseEntity", "m_iHealth");
             return new Dictionary<string, string>
             {
                 { "playerName", player.PlayerName }
             };
+        }
+
+        private void DicePlayerOneHPResetForPlayer(CCSPlayerController player)
+        {
+            if (player == null
+                || player.PlayerPawn == null
+                || !player.PlayerPawn.IsValid
+                || player.PlayerPawn.Value == null) return;
+            // reset value to the one before the dice effect
+            player.PlayerPawn.Value.Health = player.PlayerPawn.Value.MaxHealth;
         }
     }
 }
