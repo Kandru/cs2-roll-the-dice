3503567535f165771cf2d6d1ef71b55543a4f19e
Kalle <kalle@kandru.de>
2025-02-23 15:06:19 +0100

    improvement: DiceBigTaserBattery now shows status

--- src/RollTheDice+DiceBigTaserBattery.cs
+++ src/RollTheDice+DiceBigTaserBattery.cs
@@ -4,7 +4,7 @@ namespace RollTheDice
 {
     public partial class RollTheDice : BasePlugin
     {
-        private Dictionary<CCSPlayerController, int> _playersWithBigTaserBattery = new();
+        private Dictionary<CCSPlayerController, int> _playersWithBigTaserBattery = [];
 
         private Dictionary<string, string> DiceBigTaserBattery(CCSPlayerController player, CCSPlayerPawn playerPawn)
         {
@@ -19,7 +19,24 @@ namespace RollTheDice
                 Convert.ToInt32(config["max_batteries"]) + 1
             );
             _playersWithBigTaserBattery.Add(player, battery);
-            player.GiveNamedItem("weapon_taser");
+            // give taser if not exists
+            if (playerPawn.WeaponServices?.MyWeapons != null)
+            {
+                bool hasTaser = false;
+                foreach (var gun in playerPawn.WeaponServices!.MyWeapons)
+                {
+                    var weapon = gun.Value;
+                    if (weapon != null && weapon.IsValid && weapon.DesignerName.Contains("taser"))
+                    {
+                        hasTaser = true;
+                        break;
+                    }
+                }
+                if (!hasTaser)
+                {
+                    player.GiveNamedItem("weapon_taser");
+                }
+            }
             return new Dictionary<string, string>
             {
                 { "playerName", player.PlayerName },
@@ -40,8 +57,15 @@ namespace RollTheDice
 
         private void DiceBigTaserBatteryResetForPlayer(CCSPlayerController player)
         {
+            // check if player has this dice
             if (!_playersWithBigTaserBattery.ContainsKey(player)) return;
+            // remove player
             _playersWithBigTaserBattery.Remove(player);
+            // remove event listener when no players have this dice
+            if (_playersWithBigTaserBattery.Count() == 0)
+            {
+                DeregisterEventHandler<EventWeaponFire>(EventDiceBigTaserBatteryOnWeaponFire);
+            }
         }
 
         private HookResult EventDiceBigTaserBatteryOnWeaponFire(EventWeaponFire @event, GameEventInfo info)
@@ -55,6 +79,20 @@ namespace RollTheDice
             if (_playersWithBigTaserBattery[player] <= 0) return HookResult.Continue;
             player.Pawn.Value.WeaponServices.ActiveWeapon.Value!.Clip1 = 2;
             _playersWithBigTaserBattery[player]--;
+            // update gui if available
+            if (_playersThatRolledTheDice.ContainsKey(player)
+                && _playersThatRolledTheDice[player].ContainsKey("gui_status")
+                && (CPointWorldText)_playersThatRolledTheDice[player]["gui_status"] != null)
+            {
+                CPointWorldText worldText = (CPointWorldText)_playersThatRolledTheDice[player]["gui_status"];
+                if (_playersWithBigTaserBattery[player] == 0)
+                    ChangeColor(worldText, Config.GUIPositions[Config.GUIPosition].StatusColorDisabled);
+                else
+                    ChangeColor(worldText, Config.GUIPositions[Config.GUIPosition].StatusColorEnabled);
+                worldText.AcceptInput("SetMessage", worldText, worldText, Localizer["DiceBigTaserBattery_status"].Value.Replace(
+                    "{batterySize}", _playersWithBigTaserBattery[player].ToString()
+                ));
+            }
             return HookResult.Continue;
         }
 
