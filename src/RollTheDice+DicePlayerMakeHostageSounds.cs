41130edf4344676fee8b7da34ec84820c93583a4
Kalle <kalle@kandru.de>
2025-02-23 14:26:55 +0100

    fix: improve DicePlayerMakeHostageSounds and add config options

--- src/RollTheDice+DicePlayerMakeHostageSounds.cs
+++ src/RollTheDice+DicePlayerMakeHostageSounds.cs
@@ -5,7 +5,7 @@ namespace RollTheDice
 {
     public partial class RollTheDice : BasePlugin
     {
-        private readonly Dictionary<CCSPlayerController, int> _playersWithHostageSounds = new();
+        private readonly Dictionary<CCSPlayerController, Dictionary<string, int>> _playersWithHostageSounds = new();
         private readonly List<string> _hostageSounds = new List<string>
         {
             "Hostage.Pain"
@@ -16,7 +16,11 @@ namespace RollTheDice
             // create listener if not exists
             if (_playersWithHostageSounds.Count() == 0) RegisterListener<Listeners.OnTick>(EventDicePlayerMakeHostageSoundsOnTick);
             // add player to list
-            _playersWithHostageSounds.Add(player, 0);
+            _playersWithHostageSounds.Add(player, new Dictionary<string, int>
+            {
+                { "cooldown", 0 },
+                { "delay", 0 }
+            });
             return new Dictionary<string, string>
             {
                 { "playerName", player.PlayerName }
@@ -44,8 +48,8 @@ namespace RollTheDice
         {
             if (_playersWithHostageSounds.Count() == 0) return;
             // worker
-            Dictionary<CCSPlayerController, int> _playersWithHostageSoundsCopy = new(_playersWithHostageSounds);
-            foreach (var (player, playerStatus) in _playersWithHostageSoundsCopy)
+            Dictionary<CCSPlayerController, Dictionary<string, int>> _playersWithHostageSoundsCopy = new(_playersWithHostageSounds);
+            foreach (var (player, playerData) in _playersWithHostageSoundsCopy)
             {
                 try
                 {
@@ -53,15 +57,25 @@ namespace RollTheDice
                     if (player == null
                     || player.PlayerPawn == null || !player.PlayerPawn.IsValid || player.PlayerPawn.Value == null
                     || player.PlayerPawn.Value.LifeState != (byte)LifeState_t.LIFE_ALIVE) continue;
-                    if (player.Buttons == 0 && playerStatus == 0)
+                    if (player.Buttons == 0
+                        && playerData["cooldown"] > -1
+                        && playerData["cooldown"] <= (int)Server.CurrentTime
+                        || (playerData["delay"] <= (int)Server.CurrentTime))
                     {
                         // emit sound
                         EmitSound(player, _hostageSounds[_random.Next(_hostageSounds.Count)]);
-                        _playersWithHostageSounds[player] = (int)Server.CurrentTime + 1;
+                        // set random next sound delay
+                        Dictionary<string, object> config = GetDiceConfig("DicePlayerMakeHostageSounds");
+                        _playersWithHostageSounds[player]["cooldown"] = -1;
+                        _playersWithHostageSounds[player]["delay"] = (int)Server.CurrentTime + _random.Next(
+                            Convert.ToInt32(config["min_sound_delay"]),
+                            Convert.ToInt32(config["max_sound_delay"])
+                        );
                     }
-                    else if (player.Buttons != 0 && playerStatus <= (int)Server.CurrentTime)
+                    else if (player.Buttons != 0 && playerData["cooldown"] == -1)
                     {
-                        _playersWithHostageSounds[player] = 0;
+                        _playersWithHostageSounds[player]["cooldown"] = (int)Server.CurrentTime;
+
                     }
                 }
                 catch (Exception e)
@@ -72,6 +86,14 @@ namespace RollTheDice
                     Console.WriteLine(Localizer["core.error"].Value.Replace("{error}", e.Message));
                 }
             }
+        }
+
+        private Dictionary<string, object> DicePlayerMakeHostageSoundsConfig()
+        {
+            var config = new Dictionary<string, object>();
+            config["min_sound_delay"] = (int)5;
+            config["max_sound_delay"] = (int)10;
+            return config;
         }
     }
 }
