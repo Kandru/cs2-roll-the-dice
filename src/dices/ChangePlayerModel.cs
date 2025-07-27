6d40bd8b11a38688445c85107c8454228e0c616e
derkalle4 <kalle@kandru.de>
2025-07-27 12:53:38 +0200

    draft: rework ChangePlayerModel

---
+++ src/dices/ChangePlayerModel.cs
@@ -0,0 +1,84 @@
+using CounterStrikeSharp.API.Core;
+using CounterStrikeSharp.API.Modules.Utils;
+using Microsoft.Extensions.Localization;
+using RollTheDice.Utils;
+
+namespace RollTheDice.Dices
+{
+    public class ChangePlayerModel : ParentDice
+    {
+        public override string _className => "ChangePlayerModel";
+        private Dictionary<CCSPlayerController, string> _oldModels = new();
+
+        public ChangePlayerModel(PluginConfig GlobalConfig, MapConfig Config, IStringLocalizer Localizer) : base(GlobalConfig, Config, Localizer)
+        {
+            Console.WriteLine(_localizer["dice.class.initialize"].Value.Replace("{name}", _className));
+        }
+
+        public override void Add(CCSPlayerController player)
+        {
+            // check if player is valid and has a pawn
+            if (player == null
+                || !player.IsValid
+                || player.Pawn?.IsValid == false
+                || player.Pawn?.Value?.CBodyComponent?.SceneNode?.GetSkeletonInstance() == null)
+            {
+                return;
+            }
+            // get current player model
+            string playerModel = player.Pawn.Value.CBodyComponent.SceneNode.GetSkeletonInstance().ModelState.ModelName;
+            // save old model
+            _oldModels[player] = playerModel;
+            // set new player model
+            if (player.Team == CsTeam.Terrorist)
+            {
+                ChangeModel(player, "characters/models/ctm_sas/ctm_sas.vmdl");
+            }
+            else
+            {
+                ChangeModel(player, "characters/models/tm_phoenix/tm_phoenix.vmdl");
+            }
+            // create GUI for player
+            Dictionary<string, string> data = new()
+            {
+                { "playerName", player.PlayerName }
+            };
+            _players.Add(player, new Dictionary<string, CPointWorldText?>
+            {
+                { "gui", CreateMainGUI(player, _className, data) }
+            });
+        }
+
+        public override void Remove(CCSPlayerController player)
+        {
+            GUI.RemoveGUIs([.. _players[player].Values.Cast<CPointWorldText>()]);
+            ChangeModel(player, _oldModels[player]);
+            _ = _players.Remove(player);
+            _ = _oldModels.Remove(player);
+        }
+
+        public override void Destroy()
+        {
+            Console.WriteLine(_localizer["dice.class.destroy"].Value.Replace("{name}", _className));
+            // remove all GUIs for all players
+            foreach (KeyValuePair<CCSPlayerController, Dictionary<string, CPointWorldText?>> kvp in _players)
+            {
+                ChangeModel(kvp.Key, _oldModels[kvp.Key]);
+                GUI.RemoveGUIs([.. kvp.Value.Values.Cast<CPointWorldText>()]);
+            }
+            _players.Clear();
+            _oldModels.Clear();
+        }
+
+        private void ChangeModel(CCSPlayerController player, string model)
+        {
+            if (player == null
+                || player.PlayerPawn == null
+                || !player.PlayerPawn.IsValid
+                || player.PlayerPawn.Value == null
+                || player.LifeState != (byte)LifeState_t.LIFE_ALIVE) return;
+            // reset player model
+            player.PlayerPawn.Value.SetModel(model);
+        }
+    }
+}
