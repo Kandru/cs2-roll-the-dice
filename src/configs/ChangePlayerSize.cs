819d4f3f22fda55f3adf69bc94e7ce35c945db54
derkalle4 <kalle@kandru.de>
2025-08-12 21:40:45 +0200

    fix: change min and max player size to avoid bugs in cs2 engine

--- src/configs/ChangePlayerSize.cs
+++ src/configs/ChangePlayerSize.cs
@@ -5,7 +5,7 @@ namespace RollTheDice.Configs
     public class ChangePlayerSizeConfig
     {
         [JsonPropertyName("enabled")] public bool Enabled { get; set; } = true;
-        [JsonPropertyName("min_size")] public float MinSize { get; set; } = 0.1f;
-        [JsonPropertyName("max_size")] public float MaxSize { get; set; } = 2f;
+        [JsonPropertyName("min_size")] public float MinSize { get; set; } = 0.5f;
+        [JsonPropertyName("max_size")] public float MaxSize { get; set; } = 1.5f;
     }
 }
