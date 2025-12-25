5b543b9f630a26b08631ef21951ffa9031cee584
derkalle4 <kalle@kandru.de>
2025-12-25 09:20:04 +0100

    bump version to 25.12.4

--- src/Version.cs
+++ src/Version.cs
@@ -4,6 +4,6 @@ namespace RollTheDice
 {
     public partial class RollTheDice : BasePlugin
     {
-        public override string ModuleVersion => "25.12.3";
+        public override string ModuleVersion => "25.12.4";
     }
 }
