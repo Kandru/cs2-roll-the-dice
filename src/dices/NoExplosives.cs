0073a0c9beb00f96bf895e25f5be0524b538537e
derkalle4 <kalle@kandru.de>
2026-01-04 16:38:05 +0100

    improvement: set player as attacker when killing someone with a prop

--- src/dices/NoExplosives.cs
+++ src/dices/NoExplosives.cs
@@ -9,7 +9,8 @@ namespace RollTheDice.Dices
     {
         public override string ClassName => "NoExplosives";
         public override List<string> Listeners => [
-            "OnEntitySpawned"
+            "OnEntitySpawned",
+            "OnEntityTakeDamagePre"
         ];
         private readonly HashSet<string> _grenadeProjectiles =
         [
@@ -20,10 +21,17 @@ namespace RollTheDice.Dices
             "flashbang_projectile"
         ];
         public readonly Random _random = new();
+        private readonly Dictionary<nint, CCSPlayerController> _grenadesThrownByPlayers = [];
 
         public NoExplosives(PluginConfig GlobalConfig, MapConfig Config, IStringLocalizer Localizer) : base(GlobalConfig, Config, Localizer)
         {
             Console.WriteLine(_localizer["dice.class.initialize"].Value.Replace("{name}", ClassName));
+        }
+
+        public override void Reset()
+        {
+            _players.Clear();
+            _grenadesThrownByPlayers.Clear();
         }
 
         public void OnEntitySpawned(CEntityInstance entity)
@@ -37,6 +45,28 @@ namespace RollTheDice.Dices
             {
                 DiceNoExplosivesHandle(entity.Handle);
             }
+        }
+
+        public HookResult OnEntityTakeDamagePre(CBaseEntity entity, CTakeDamageInfo info)
+        {
+            if (entity is null
+                || !entity.IsValid
+                || info.Inflictor is null
+                || !info.Inflictor.IsValid
+                || info.Inflictor.Value is null
+                || !info.Inflictor.Value.IsValid
+                // check if grenade was thrown by player
+                || !_grenadesThrownByPlayers.ContainsKey(info.Inflictor.Value.Handle))
+            {
+                return HookResult.Continue;
+            }
+            nint inflictorHandler = info.Inflictor.Value.Handle;
+            // change to proper attacker (otherwise counts as self-kill)
+            info.Attacker.Raw = _grenadesThrownByPlayers[inflictorHandler].Pawn.Raw;
+            // change to headshot (looks better in the killfeed)
+            info.BitsDamageType = DamageTypes_t.DMG_HEADSHOT;
+            // update the result
+            return HookResult.Changed;
         }
 
         private void DiceNoExplosivesHandle(nint handle)
@@ -66,8 +96,7 @@ namespace RollTheDice.Dices
                         return;
                     }
                     string Model = _config.Dices.NoExplosives.RandomModels[new Random().Next(_config.Dices.NoExplosives.RandomModels.Count)];
-                    // create physics model
-                    _ = CreatePhysicsModel(
+                    nint inflictorHandler = CreatePhysicsModel(
                         Model,
                         _config.Dices.NoExplosives.ModelScale,
                         grenade.AbsOrigin,
@@ -76,33 +105,42 @@ namespace RollTheDice.Dices
                             grenade.Velocity.X,
                             grenade.Velocity.Y,
                             grenade.Velocity.Z
-                        )
-                    );
+                        ));
+                    // create physics model and add to list
+                    _grenadesThrownByPlayers.Add(
+                        inflictorHandler,
+                        owner.Controller.Value.As<CCSPlayerController>());
+                    // remove entry after some time, but delay because we wait for eventual damage
+                    _ = new CounterStrikeSharp.API.Modules.Timers.Timer(10f, () =>
+                    {
+                        _grenadesThrownByPlayers.Remove(inflictorHandler);
+                    });
                     // stop sound and kill grenade
                     _ = grenade.EmitSound("StopSoundEvents.StopAllExceptMusic");
+                    // kill original grenade
                     grenade.AcceptInput("Kill");
                 }
             });
         }
 
-        private static uint CreatePhysicsModel(string model, float scale, Vector origin, QAngle angles, Vector velocity)
+        private static nint CreatePhysicsModel(string model, float scale, Vector origin, QAngle angles, Vector velocity)
         {
             // create physics prop
-            CDynamicProp prop;
-            prop = Utilities.CreateEntityByName<CDynamicProp>("prop_physics_override")!;
+            CPhysicsProp prop;
+            prop = Utilities.CreateEntityByName<CPhysicsProp>("prop_physics_override")!;
             // settings
             prop.Health = 10;
             prop.MaxHealth = 10;
-            // set model
-            prop.SetModel(model);
             // scale
             CEntityKeyValues kv = new();
-            kv.SetFloat("modelscale", scale);
+            kv.SetFloat("modelscale", 5f);
             // spawn it
             prop.DispatchSpawn(kv);
+            // set model
+            prop.SetModel(model);
             // teleport properly
             prop.Teleport(origin, angles, velocity);
-            return prop.Index;
+            return prop.Handle;
         }
     }
 }
